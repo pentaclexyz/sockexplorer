f1a28ceee27d433ccd87b24f33a900c9a3c2895d
“0xpentacle” <0xpentacle@pentacle.ai>
2021-05-30 02:04:27 +1200

    small progress

--- src/pages/socks/bubble/bubble.js
+++ src/pages/socks/bubble/bubble.js
@@ -18,32 +18,30 @@ export default function define(runtime, observer) {
             const strength_var_1 = 0.5;
             const strength_var_2 = 0.8;
 
-            let svg = null;
-            let nodes = [];
-
             const simulation = d3.forceSimulation()
                 .force('charge', d3.forceManyBody().strength(strength_var_1))
-                // .velocityDecay(0.4)
+                .velocityDecay(0.4)
                 .force('center', d3.forceCenter(centre.x, centre.y))
                 .force('x', d3.forceX(width / 2).strength(forceStrength).x(centre.x))
                 .force('y', d3.forceY(height / 2).strength(forceStrength).y(centre.y))
                 .force('collision', d3.forceCollide().radius(d => d.radius + 0.1).strength(strength_var_2));
 
-            var tooltip = d3.select(".wallet-info")
+            const tooltip = d3.select(".wallet-info")
                 .style("opacity", 0)
                 .attr("class", "tooltip")
 
-            var showTooltip = function(d) {
+            const showTooltip = function(d) {
                 tooltip
                     .style("opacity", 1)
                     .style("background-color", "pink")
                     .style("border", "solid")
                     .style("border-radius", ".5rem")
                     .style("padding", ".75rem")
-                    .text(root, d => d.data.wallet) // trying to get it to display some text!!
+                    .text(root, d => d.wallet) // trying to get it to display some text!!
                     // .html("Wallet Address: " + d.wallet + "<br>" + "Token Balance: " + d.value + "<br>" + "Initial Transaction Date: " + d.buydate+ "<br>" + "Last Transaction Date: " + d.lastdate)
             }
-            var hideTooltip = function(d) {
+
+            const hideTooltip = function(d) {
                 tooltip
                     .style("opacity", 0)
 
@@ -52,7 +50,7 @@ export default function define(runtime, observer) {
                     .style("opacity", 1)
             }
 
-            svg = d3.create("svg")
+            const svg = d3.create("svg")
                 .attr("viewBox", [0, 0, width, height])
                 .attr("text-anchor", "middle");
 
@@ -63,13 +61,8 @@ export default function define(runtime, observer) {
 
             const g = svg.append("g");
 
-            // const leaf = g.selectAll("g")
-            //     .data(root.leaves())
-            //     .join("g")
-            //     .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
-
             const leaf = g.selectAll('g')
-                .data(root, d => d.wallet)
+                .data(root.leaves())
                 .join("g")
                 .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
 
@@ -81,10 +74,10 @@ export default function define(runtime, observer) {
                 .attr("stroke-width", 0.1)
                 .attr("fill-opacity", 1)
                 .attr("fill", d => color(d.value))
-                .attr("xlink:href",  function(d) { return d.url;})
+                .attr("xlink:href",  function(d) { return d.data.url;})
                 .on("mouseover", showTooltip)
                 .on("mouseleave", hideTooltip)
-                .on('click', function(d) {(window.open(d.url, '_blank'))})
+                .on('click', function(d) {(window.open(d.data.url, '_blank'))})
 
             simulation.on("tick", () => {
                 console.log(root.leaves());
@@ -98,7 +91,6 @@ export default function define(runtime, observer) {
 
             invalidation.then(() => simulation.stop());
 
-
             function zoomed({transform}) {
                 g.attr("transform", transform);
             }
@@ -106,9 +98,11 @@ export default function define(runtime, observer) {
             return svg.node();
         }
     );
+
     main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){return(
         FileAttachment("socks-holders.json").json()
     )});
+
     main.variable(observer("pack")).define("pack", ["d3","width","height"], function(d3,width,height){return(
         data => d3.pack()
             .size([width - 2, height - 2])

a3c0b8918cb8cf3d1ab2cfe8f3895664f2b13362
“0xpentacle” <0xpentacle@pentacle.ai>
2021-05-31 11:09:51 +1200

    donate link

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
         return <footer>
             <article className={"footer-content flex-group space-between flex-align-end"}>
                 <div>
-                    <a href={"https://pentacle.ai"} target={"_blank"} rel="noopener noreferrer">Made by pentacle dot ai</a>
+                    <a href={"https://pentacle.ai"} target={"_blank"} rel="noopener noreferrer">Made by Pentacle</a>
                     <a href={"https://etherscan.io/address/pentacle.eth"} target={"_blank"} rel="noopener noreferrer">Donate to pentacle.eth</a>
                     <a href={"https://github.com/pentacledotai/sockexplorer"} target={"_blank"} rel="noopener noreferrer">GitHub</a>
                 </div>

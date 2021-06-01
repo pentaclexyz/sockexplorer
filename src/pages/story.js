0a6cf5f5a2b08b35d37d534c66cbac3861707d70
“0xpentacle” <0xpentacle@pentacle.ai>
2021-06-01 12:33:43 +1200

    kill twitter embeds

--- src/pages/story.js
+++ src/pages/story.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
 import Header from "../components/header";
-import {TwitterTweetEmbed} from 'react-twitter-embed'
 
 
 export const Story = () => {
@@ -11,16 +10,16 @@ export const Story = () => {
             <Layout>
                 <section className={"editorial-content one-col"}>
 
-                    <article className={"box feature box-padding flex-group"}>
-                        <div>
-                            <TwitterTweetEmbed tweetId={"1126506339075641344"}/>
-                            <TwitterTweetEmbed tweetId={"1392526628438302721"}/>
-                        </div>
-                        <div>
-                            <TwitterTweetEmbed tweetId={"1398068623269191681"}/>
-                            <TwitterTweetEmbed tweetId={"1220875073252401153"}/>
-                        </div>
-                    </article>
+                    {/*<article className={"box feature box-padding flex-group"}>*/}
+                    {/*    <div>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1126506339075641344"}/>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1392526628438302721"}/>*/}
+                    {/*    </div>*/}
+                    {/*    <div>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1398068623269191681"}/>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1220875073252401153"}/>*/}
+                    {/*    </div>*/}
+                    {/*</article>*/}
 
                     <article className={"box feature box-padding"}>
 
@@ -30,26 +29,24 @@ export const Story = () => {
                         <p>Each $SOCK redeemed from the pool increases the price of the remaining ones, creating the
                             following price curve. The price of $ETH at the time of the original release was $170.</p>
 
-                        <img src={"https://pbs.twimg.com/media/D6IjysLWsAAE_Xa?format=png"}/>
+                        <img src={"https://pbs.twimg.com/media/D6IjysLWsAAE_Xa?format=png"} alt={"price chart"}/>
 
                         <p>$SOCKS can be sold back into the pool to receive $ETH according to the same formula, or
                             burned in order to redeem one pair of physical Unisocks.</p>
                     </article>
 
-                    <article className={"box feature box-padding flex-group"}>
-                        <div>
-                            <TwitterTweetEmbed tweetId={"1143233945053663232"}/>
-                        </div>
-                        <div>
-                            <TwitterTweetEmbed tweetId={"1395496386318290952"}/>
-                        </div>
-                    </article>
+                    {/*<article className={"box feature box-padding flex-group"}>*/}
+                    {/*    <div>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1143233945053663232"}/>*/}
+                    {/*    </div>*/}
+                    {/*    <div>*/}
+                    {/*        <TwitterTweetEmbed tweetId={"1395496386318290952"}/>*/}
+                    {/*    </div>*/}
+                    {/*</article>*/}
                     <article className={"box feature box-padding margin-bottom-5"}>
-                        <p className={"feature"}>At the time of writing, 19 $SOCKS remain in the original pool,
-                            redeemable for the price of $55k
-                            or 35 $ETH</p>
+                        <p className={"feature"}>At the time of writing, 20 $SOCKS remain in the <a href={"https://unisocks.exchange/"} target={"_blank"} rel="noopener noreferrer">original pool</a>, with a current price for the next one at $62,366k</p>
                         <p>A total of 305 $SOCKS are in circulation. Besides redeeming them from the
-                            bonding curve pool, $SOCKS can be traded on secondary markets, such as regular xyk pools on
+                            bonding curve pool, $SOCKS can be traded on secondary markets, such as regular <a href={"https://info.uniswap.org/#/pools/0xc75a99fa00803896349891f8471da3614bd07564"} target={"_blank"} rel="noopener noreferrer">xyk pools</a> on
                             Uniswap.</p>
                         <p>There are currently ~10 $SOCKS of liquidity in the UniV3 SOCKS-ETH pool, and
                             another 1 $SOCK in the same UniV2 pool.</p>

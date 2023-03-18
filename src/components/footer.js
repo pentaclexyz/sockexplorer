df001387d9e714fb4546527d56966b62f4550e15
pentacle <pentacle@pentacle.xyz>
2023-03-18 21:32:22 +0000

    fix: footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,8 @@ class Footer extends Component {
             <article className={"footer-content flex-group space-between flex-align-end"}>
                 <div>
                     <a href={"https://app.safe.global/home?safe=eth:0x8EFEF51d19EF3844C00076ab9d02847B9C70f94A"} target={"_blank"} rel="noopener noreferrer">Donate to pentamom.eth</a>
-                    <a href={"https://pentacle.ai"} target={"_blank"} rel="noopener noreferrer">Made by Pentacle</a>
-                    <a href={"https://github.com/pentacledotai/sockexplorer"} target={"_blank"} rel="noopener noreferrer">GitHub</a>
+                    <a href={"https://pentacle.ai"} target={"_blank"} rel="noopener noreferrer">Made by The Penthouse</a>
+                    <a href={"https://github.com/pentaclexyz/sockexplorer"} target={"_blank"} rel="noopener noreferrer">GitHub</a>
                 </div>
                 <div className={"embed"}>
                     <iframe width="100%" height="120" title={"miyuki eto"}
